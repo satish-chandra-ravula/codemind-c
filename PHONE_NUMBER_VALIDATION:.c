58ed43c616cc7e0c7442fd9e4a9a14aa01a17acc
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2022-12-07 11:10:43 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a>=100000000&& a<=9999999999) {
            printf("Valid");
        }
        else 
        {
            printf("Invalid");}
        
    }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=100000000&& a<=9999999999)
+{
+    
+    
+        printf("Valid");
+    }
+    else 
+    {
+        printf("Invalid");}
+    
+}
