a63e1a6456a9f470f42b60ab2fd498938d635783
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2022-12-12 11:03:06 +0530

    #include<stdio.h> int main() {     int ar[3],i,p=1;     for(i=0;i<3;i++)     {         scanf("%d",&ar[i]);     } for(i=0;i<3;i++) p=p*ar[i]; {     printf("%dKB",p); } }

---
+++ Find_the_Capacity.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int ar[3],i,p=1;
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+for(i=0;i<3;i++)
+p=p*ar[i];
+{
+    printf("%dKB",p);
+}
+}
