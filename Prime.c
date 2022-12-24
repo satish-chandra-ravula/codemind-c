e3a6102432ea5ba15da2053233e84de40e1d6c97
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2022-12-24 19:59:45 +0530

    #include<stdio.h> int main() {     int a,i,count=0;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         if(a%i==0)         {             count+=1;
            }
            }
            if(count==2) 
            { printf("Prime");
            }
            else 
            { printf("Not Prime"); }
                
            }

---
+++ Prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,i,count=0; 
+    scanf("%d",&a); 
+    for(i=1;i<=a;i++) 
+    {
+        if(a%i==0) 
+        { 
+            count+=1; 
+            
+        }
+        }
+        if(count==2) 
+        { printf("Prime");
+        }
+        else 
+        { printf("Not Prime"); }
+            
+        }
+        
+        
